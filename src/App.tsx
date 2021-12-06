74ae5a2f3eab61776fc91546254990a32d321a3a
pedrobarretto <pedro@barretto.com.br>
2021-12-06 11:07:43 -0300

    Install styled-components, config global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      <h1>stro</h1>
+      <h1>stro oho</h1>
+      <GlobalStyle />
     </div>
   );
 }
